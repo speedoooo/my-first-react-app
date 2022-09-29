27255e41122d5dee4fb1c74347f2fb9ef683497d
ide__ <hayato.ebios@outlook.jp>
2022-09-30 08:15:24 +0900

    選択したアイテムをボックスに追加

--- components/BoxItems.jsx
+++ components/BoxItems.jsx
@@ -1,10 +1,46 @@
+import { useEffect, useState } from "react";
 const BoxItems = () => {
-  // ここにjsかく
+  const [item,setItem] = useState([])
+  // 配列を宣言
+  console.log('a');
+  const array = [];
+  useEffect(()=>{
+      console.log('実行');
+      if (process.browser) {
+        array = []
+        for(let key in localStorage){
+          const data = localStorage.getItem(key);
+          console.log(data);
+          if(data!=null){
+            const boxItemData = data.split(',');
+            console.log(boxItemData);
+            // 配列に格納
+            if(boxItemData.length == 2){
+              array.push(boxItemData);
+
+            }
+          }
+        }
+      console.log(array);
+      setItem(array)
+
+    }
+  },[setItem])
+
   return (
     <>
       <h1>Boxページ</h1>
       {/* アイテムリスト */}
+      {item.map((it, i)=>(
+        <div key={i}>
+          <img src="image.png"/>
+          <p>{it[0]}</p>
+          <p>{it[1]}</p>
+        </div>
+      ))}
+
       {/* レンタルボタン */}
+      <button type="button">レンタルする</button>
     </>
   );
 };
--- components/Items.jsx
+++ components/Items.jsx
@@ -1,11 +1,46 @@
 const Items = () => {
-  // ここにjsかく
+  function saveitems() {
+    if (process.browser) {
+      localStorage.clear();
+      const mydata = document.getElementsByName("item");
+      // console.log(mydata);
+      const sumItems=0;
+      for(let i = 0; i < mydata.length; i++){
+        if(mydata[i].checked){
+          localStorage.setItem(sumItems, mydata[i].value);
+          sumItems++;
+        }
+      }
+  
+      console.log(localStorage);
+    }
+  }
   return (
     <>
       <h1>コーディネート詳細ページ</h1>
+
       {/* アイテムグリッド＋チェックボックス */}
+      <img src="image.png"/><input type="checkbox" name="item" value="3WAY MILITARY BLOUSON(SHORT M-65),1000"/>
+        <h2>3WAY MILITARY BLOUSON(SHORT M-65)</h2>
+        <h3>¥1000</h3>
+      <img src="image.png"/><input type="checkbox" name="item" value="LAYERED STRETCH TURTLE,2000"/>
+        <h2>LAYERED STRETCH TURTLE</h2>
+        <h3>¥2000</h3>
+      <img src="image.png"/><input type="checkbox" name="item" value="LOOSE FIT DENIM,3000"/>
+        <h2>LOOSE FIT DENIM</h2>
+        <h3>¥3000</h3>
+      <img src="image.png"/><input type="checkbox" name="item" value="COMBINATION NECKLACE,4000"/>
+        <h2>COMBINATION NECKLACE</h2>
+        <h3>¥4000</h3>
+      <img src="image.png"/><input type="checkbox" name="item" value="SHARK SOLE COMBINATION SHOES,5000"/>
+        <h2>SHARK SOLE COMBINATION SHOES</h2>
+        <h3>¥5000</h3>
+
       {/* BOX追加ボタン */}
+      <button type="submit" onClick={()=>saveitems()}>BOXに入れる</button>
+
       {/* BOX見るリンク */}
+      <a href="box">BOXを見る</a>
     </>
   );
 };
